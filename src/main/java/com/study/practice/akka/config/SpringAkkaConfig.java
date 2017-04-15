ac1b698b23b32138e4878ebe21565af73679b7cb
blessldk <lovmistake@gmail.com>
2017-04-15 16:47:27 +0900

    [create] spring akka configuration init

---
+++ src/main/java/com/study/practice/akka/config/SpringAkkaConfig.java
@@ -0,0 +1,10 @@
+package com.study.practice.akka.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages={"com.study.practice.akka"})
+public class SpringAkkaConfig {
+    
+}
