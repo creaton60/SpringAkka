a3fccb4685886807634bb7581c5c5ffe4b9fdec9
blessldk <lovmistake@gmail.com>
2017-04-15 16:36:02 +0900

    [create] project init

---
+++ src/main/java/com/study/practice/SpringBootAkkaApplication.java
@@ -0,0 +1,12 @@
+package com.study.practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAkkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAkkaApplication.class, args);
+	}
+}
